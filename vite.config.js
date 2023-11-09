766cce16f1f5d1fa2822e36b91e987c81675fd51
ugrair <irina.ugrak@gmail.com>
2023-11-09 19:51:22 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,23 @@
-// import Inspect from 'vite-plugin-inspect';
-// import { resolve } from "path";
-// import { defineConfig } from 'vite';
+import Inspect from 'vite-plugin-inspect';
+import { resolve } from "path";
+import { defineConfig } from 'vite';
 
-// export default defineConfig({
-//   base: "/socratica/",
-//   plugins: [Inspect()],
-//   build: {
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'index.html'),
-//         page1: resolve(__dirname, 'page1/index.html'),
-//         page2: resolve(__dirname, 'page2/index.html'),
-//         page3: resolve(__dirname, 'page3/index.html'),
-//         page4: resolve(__dirname, 'page4/index.html'),
-//         page5: resolve(__dirname, 'page5/index.html'),
-//         page6: resolve(__dirname, 'page6/index.html')
-//       }
-
-//     }
-//   }
-// })
-
-import { defineConfig } from 'vite'
-
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/socratica",
-  plugins: [],
+  plugins: [Inspect()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        page1: resolve(__dirname, 'page1/index.html'),
+        page2: resolve(__dirname, 'page2/index.html'),
+        page3: resolve(__dirname, 'page3/index.html'),
+        page4: resolve(__dirname, 'page4/index.html'),
+        page5: resolve(__dirname, 'page5/index.html'),
+        page6: resolve(__dirname, 'page6/index.html')
+      }
+
+    }
+  }
 })
+
