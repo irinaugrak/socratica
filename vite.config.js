aa4cd5dbe9143227e7682c1f178acb3914d2605f
ugrair <irina.ugrak@gmail.com>
2023-11-09 19:45:32 +0300

    vite.config changed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/socratica/",
+  base: "https://irinaugrak.github.io/socratica/",
   plugins: [Inspect()],
   build: {
     rollupOptions: {
