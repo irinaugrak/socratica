4c57c21102890ca847475af216c0f9cf7407dd2d
ugrair <irina.ugrak@gmail.com>
2023-11-09 19:11:59 +0300

    gh-pages change

--- vite.config.js
+++ vite.config.js
@@ -8,13 +8,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        page1: resolve(__dirname, 'page1/index.html'),
-        page2: resolve(__dirname, 'page2/index.html'),
-        page3: resolve(__dirname, 'page3/index.html'),
-        page4: resolve(__dirname, 'page4/index.html'),
-        page5: resolve(__dirname, 'page5/index.html'),
-        page6: resolve(__dirname, 'page6/index.html')
+        main: resolve(__dirname, '/index.html'),
+        page1: resolve(__dirname, '/page1/index.html'),
+        page2: resolve(__dirname, '/page2/index.html'),
+        page3: resolve(__dirname, '/page3/index.html'),
+        page4: resolve(__dirname, '/page4/index.html'),
+        page5: resolve(__dirname, '/page5/index.html'),
+        page6: resolve(__dirname, '/page6/index.html')
       }
 
     }
