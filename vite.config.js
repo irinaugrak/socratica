05ff7a87e28a0e5834b0b8b9db298be79de0ff07
ugrair <irina.ugrak@gmail.com>
2023-11-09 15:19:55 +0300

    Added Socratica project

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import Inspect from 'vite-plugin-inspect';
+import { resolve } from "path";
+
+export default {
+  plugins: [Inspect()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        page1: resolve(__dirname, 'page1/index.html'),
+        page2: resolve(__dirname, 'page2/index.html'),
+        page3: resolve(__dirname, 'page3/index.html'),
+        page4: resolve(__dirname, 'page4/index.html'),
+        page5: resolve(__dirname, 'page5/index.html')
+      }
+
+    }
+  }
+}
