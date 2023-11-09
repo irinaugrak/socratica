5d2b498ebcf97617a052011ecb00d9429db7704e
ugrair <irina.ugrak@gmail.com>
2023-11-09 19:20:28 +0300

    gh-pages changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "/socratica",
   plugins: [Inspect()],
   build: {
     rollupOptions: {
