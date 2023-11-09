4e5c837f2f0a18ad535891bf04607dc3ed2bb665
ugrair <irina.ugrak@gmail.com>
2023-11-09 18:12:19 +0300

    Added new pages

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default {
         page2: resolve(__dirname, 'page2/index.html'),
         page3: resolve(__dirname, 'page3/index.html'),
         page4: resolve(__dirname, 'page4/index.html'),
-        page5: resolve(__dirname, 'page5/index.html')
+        page5: resolve(__dirname, 'page5/index.html'),
+        page6: resolve(__dirname, 'page6/index.html')
       }
 
     }
